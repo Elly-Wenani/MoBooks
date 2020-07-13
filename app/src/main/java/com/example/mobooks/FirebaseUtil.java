9550b529beab31d712fc6cc6f983473fffd6aeba
Elly Wenani <ellywenani@gmail.com>
2020-07-14 01:47:58 +0300

    Created FirebaseUtil class

---
+++ app/src/main/java/com/example/mobooks/FirebaseUtil.java
@@ -0,0 +1,137 @@
+package com.example.mobooks;
+
+import android.app.Activity;
+import android.util.Log;
+import android.widget.Toast;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+
+import com.firebase.ui.auth.AuthUI;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.ChildEventListener;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageReference;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+import static androidx.core.app.ActivityCompat.startActivityForResult;
+
+public class FirebaseUtil {
+
+    public static FirebaseDatabase mFirebaseDatabase;
+    public static DatabaseReference mDatabaseReference;
+    private static FirebaseUtil mFirebaseUtil;
+    public static FirebaseAuth mFirebaseAuth;
+    public static FirebaseStorage mStorage;
+    public static StorageReference mStorageReference;
+    public static FirebaseAuth.AuthStateListener mAuthStateListener;
+    public static ArrayList<BooksMode> mDeals;
+    private static final int RC_SIGN_IN = 123;
+    private static BusinessActivity caller;
+
+    private FirebaseUtil() {
+    }
+
+    public static boolean isAdmin;
+
+    public static void openFbReference(String ref, final BusinessActivity callerActivity) {
+        if (mFirebaseUtil == null) {
+            mFirebaseUtil = new FirebaseUtil();
+            mFirebaseDatabase = FirebaseDatabase.getInstance();
+
+            mFirebaseAuth = FirebaseAuth.getInstance();
+            caller = callerActivity;
+
+            mAuthStateListener = new FirebaseAuth.AuthStateListener() {
+                @Override
+                public void onAuthStateChanged(@NonNull FirebaseAuth firebaseAuth) {
+                    if (firebaseAuth.getCurrentUser() == null) {
+                        FirebaseUtil.singIn();
+                    } else {
+                        String userId = firebaseAuth.getUid();
+                        checkAdmin(userId);
+                    }
+                    Toast.makeText(callerActivity.getBaseContext(), "Welcome back", Toast.LENGTH_SHORT).show();
+                }
+            };
+            connectStorage();
+        }
+        mDeals = new ArrayList<BooksMode>();
+        mDatabaseReference = mFirebaseDatabase.getReference().child(ref);
+    }
+
+    public static void singIn() {
+        // Choose authentication providers
+        List<AuthUI.IdpConfig> providers = Arrays.asList(
+                new AuthUI.IdpConfig.PhoneBuilder().build(),
+                new AuthUI.IdpConfig.EmailBuilder().build(),
+                new AuthUI.IdpConfig.GoogleBuilder().build());
+
+// Create and launch sign-in intent
+        caller.startActivityForResult(
+                AuthUI.getInstance()
+                        .createSignInIntentBuilder()
+                        .setAvailableProviders(providers)
+                        .setTheme(R.style.AppTheme_NoActionBar)
+                        .setLogo(R.drawable.logo)
+                        .build(),
+                RC_SIGN_IN);
+    }
+
+    private static void checkAdmin(String uId) {
+        FirebaseUtil.isAdmin = false;
+        DatabaseReference ref = mFirebaseDatabase.getReference().child("administrators")
+                .child(uId);
+
+        ChildEventListener listener = new ChildEventListener() {
+            @Override
+            public void onChildAdded(@NonNull DataSnapshot snapshot, @Nullable String previousChildName) {
+                FirebaseUtil.isAdmin = true;
+                //Log.d("Admin: ", "You are an admin");
+                caller.showMenu();
+            }
+
+            @Override
+            public void onChildChanged(@NonNull DataSnapshot snapshot, @Nullable String previousChildName) {
+
+            }
+
+            @Override
+            public void onChildRemoved(@NonNull DataSnapshot snapshot) {
+
+            }
+
+            @Override
+            public void onChildMoved(@NonNull DataSnapshot snapshot, @Nullable String previousChildName) {
+
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+
+            }
+        };
+        ref.addChildEventListener(listener);
+    }
+
+    public static void attachListener() {
+        mFirebaseAuth.addAuthStateListener(mAuthStateListener);
+    }
+
+    public static void detachListener() {
+        mFirebaseAuth.removeAuthStateListener(mAuthStateListener);
+    }
+
+    public static void connectStorage(){
+        mStorage = FirebaseStorage.getInstance();
+        mStorageReference = mStorage.getReference().child("deals_pictures");
+    }
+
+}
