24463db469251e8b9d2f672aa47c49975cae254d
Elly Wenani <ellywenani@gmail.com>
2020-07-14 00:50:03 +0300

    Added BooksMode class

---
+++ app/src/main/java/com/example/mobooks/BooksMode.java
@@ -0,0 +1,71 @@
+package com.example.mobooks;
+
+import java.io.Serializable;
+
+public class BooksMode implements Serializable {
+    private String id;
+    private String title;
+    private String description;
+    //private String price;
+    private String imageUrl;
+    private String imageName;
+
+    public BooksMode(){}
+
+    public BooksMode(String title, String description, String imageUrl, String imageName) {
+        this.setId(id);
+        this.setTitle(title);
+        this.setDescription(description);
+        //this.setPrice(price);
+        this.setImageUrl(imageUrl);
+        this.setImageName(imageName);
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+//    public String getPrice() {
+//        return price;
+//    }
+//
+//    public void setPrice(String price) {
+//        this.price = price;
+//    }
+
+    public String getImageUrl() {
+        return imageUrl;
+    }
+
+    public void setImageUrl(String imageUrl) {
+        this.imageUrl = imageUrl;
+    }
+
+    public String getImageName() {
+        return imageName;
+    }
+
+    public void setImageName(String imageName) {
+        this.imageName = imageName;
+    }
+}
